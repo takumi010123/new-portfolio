2697ce2d2be65dd5fa3b729a9cce2602a3783833
blueberry010123 <blueberrydrops1010@gmail.com>
2022-06-16 11:37:24 +0900

    ヘッダー作成

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,1 +1,1 @@
-document.querySelector(".menu-btn").addEventListener("click",(function(){document.querySelector(".menu").classList.toggle("is-active")}));
+function hamburger(){document.getElementById("line1").classList.toggle("line_1"),document.getElementById("line2").classList.toggle("line_2"),document.getElementById("line3").classList.toggle("line_3"),document.getElementById("nav").classList.toggle("in")}document.getElementById("hamburger").addEventListener("click",(function(){hamburger()}));
